935e04f22393b8388b5ddc44b94e2631835597b5
Murgnar2 <johannes.gale@gmail.com>
2022-12-09 15:42:48 +0200

    mid chapter6 update2.1

---
+++ Data/meet_and_repeat.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(tidyr)
+
+BPRS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep  =" ", header = T)
+RATS <- read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", header = TRUE, sep = '\t')
+
+str(BPRS)
+str(RATS)
+
+names(BPRS)
+
+dim(BPRS)
+dim(RATS)
+
+summary(BPRS)
+summary(RATS)
+
+BPRS$treatment <- factor(BPRS$treatment)
+BPRS$subject <- factor(BPRS$subject)
+
+RATS$ID <- factor(RATS$ID)
+RATS$Group <- factor(RATS$Group)
+
+RATSL <- pivot_longer(RATS, cols = -c(ID, Group), 
+                      names_to = "WD",
+                      values_to = "Weight") %>% 
+  mutate(Time = as.integer(substr(WD, 3,4))) %>%
+  arrange(Time)
+
+BPRSL <-  pivot_longer(BPRS, cols = -c(treatment, subject),
+                       names_to = "weeks", values_to = "bprs") %>%
+  arrange(weeks)
+
+
